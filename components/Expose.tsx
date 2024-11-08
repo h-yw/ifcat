57e61c9b9c35623b4cbb31b8bff86ad6eb5e986b
h-yw <1327603193@qq.com>
2024-11-09 00:41:26 +0800

    fix: 修正服务端上报造成路有卡顿问题

--- components/Expose.tsx
+++ components/Expose.tsx
@@ -1,16 +1,19 @@
-import { createClient } from 'utils/supabase/server'
-export default async function Expose({ params }: { params: { path: string; title: string } }) {
+'use client'
+import { useEffect } from 'react'
+import { createClient } from 'utils/supabase/client'
+export default function Expose({ params }: { params: { path: string; title: string } }) {
   const isProduction = process.env.NODE_ENV === 'production'
-  if (!params?.path || !isProduction) return
-  const supabase = await createClient()
-  try {
+  async function updateViews() {
+    if (!params?.path || !isProduction) return
+    const supabase = await createClient()
     await supabase.rpc('increment', {
       _path: params.path,
       _title: params.title,
       _filed: 'expose',
     })
-  } catch (e) {
-    // 上报异常
   }
+  useEffect(() => {
+    updateViews()
+  }, [])
   return <></>
 }
